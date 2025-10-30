295d843398b2fa67b45d01627a4fa3c378d4c730
ceciliachris <cecilia.christianssons@gmail.com>
2025-10-30 07:31:45 +0100

    Initial commit

---
+++ src/test/java/se/cecilia/Bookingservice/BookingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.cecilia.Bookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
